8ff12a1104fba392dee9c6c4c1a6893b389225de
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2024-04-02 14:43:51 +0800

    Update patches.sh

--- patches.sh
+++ patches.sh
@@ -1,3 +1,27 @@
+patch_files=(	
+    fs/exec.c	
+    fs/open.c	
+    fs/read_write.c	
+    fs/stat.c	
+)	
+
+is_ksu=0
+
+for i in "${patch_files[@]}"; do	
+    if grep -q "ksu" "$i"; then	
+        echo "Warning: $i contains KernelSU"	
+            if grep -q "CONFIG_KERNELSU" "$i"; then	
+                sed -i s/'CONFIG_KERNELSU'/'CONFIG_KSU'/g "$i"	
+                echo "CONFIG_KERNELSU  ---->  CONFIG_KSU"	
+            fi	
+    fi
+    is_ksu=1
+done
+
+if [ $is_ksu == "1"  ];then
+   retuen
+fi
+
 if git apply --check ../KernelSU.patch ; then
    git apply ../KernelSU.patch
 fi
